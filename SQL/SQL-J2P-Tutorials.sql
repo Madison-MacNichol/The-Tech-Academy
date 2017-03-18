967197784892fa73171fefcedc9e69cb1c95ea8c
Madison MacNichol <madimac711@gmail.com>
2017-03-18 16:44:46 -0700

    up until video 5.1

--- SQL/SQL-J2P-Tutorials.sql
+++ SQL/SQL-J2P-Tutorials.sql
@@ -3,6 +3,7 @@ SELECT*
 FROM [Grant]
 WHERE GrantName like '_%@%_.com' 
 
+/*INNER JOINS!*/
 /*2 tables in 1 from clause*/
 SELECT*
 FROM Employee
@@ -14,7 +15,7 @@ SELECT*
 FROM Employee inner join Location
 ON Employee.LocationID=Location.LocationID
 
-/* another example*/
+/*another example*/
 SELECT*
 FROM Employee
 
@@ -38,3 +39,58 @@ ON Employee.LocationID=Location.LocationID
 SELECT FirstName,LastName,GrantName,Amount
 FROM Employee INNER JOIN [Grant]
 ON Employee.EmpID=[Grant].EmpID
+
+/*OUTER JOINS*/
+SELECT FirstName,LastName,City,State
+FROM Employee LEFT OUTER JOIN Location
+ON Employee.LocationID=Location.LocationID
+
+SELECT FirstName,LastName,City,State
+FROM Employee RIGHT OUTER JOIN Location
+ON Employee.LocationID=Location.LocationID
+
+SELECT FirstName,LastName,City,State
+FROM Employee FULL OUTER JOIN Location
+ON Employee.LocationID=Location.LocationID
+
+/*Table aliases*/
+SELECT FirstName, LastName, City, Location.State
+FROM Employee INNER JOIN Location 
+ON Employee.LocationID=Location.LocationID
+/*goes to*/
+
+/*CROSS JOINS*/
+SELECT*
+FROM MgmtTraining
+
+SELECT*
+FROM Employee
+WHERE EmpID IN (1,2)
+/*goes to*/
+SELECT*
+FROM Employee CROSS JOIN MgmtTraining
+WHERE EmpID IN (1,2)
+
+/*another example*/
+SELECT FirstName,LastName, City,L.State
+FROM Employee AS E CROSS JOIN Location AS L
+
+/*UNMATCHED QUERIES*/
+SELECT*
+FROM Location AS L INNER JOIN Employee AS E
+ON L.LocationID=E.LocationID
+/*goes to*/
+SELECT*
+FROM Location AS L LEFT OUTER JOIN Employee AS E
+ON L.LocationID=E.LocationID
+/*goes to*/
+SELECT*
+FROM Location AS L LEFT OUTER JOIN Employee AS E
+ON L.LocationID=E.LocationID
+WHERE E.EmpID IS NULL
+
+/*another example*/
+SELECT E.FirstName,E.LastName,G.GrantName
+FROM Employee AS E LEFT OUTER JOIN [Grant] AS G
+ON E.EmpID=G.EmpID
+WHERE G.GrantName IS NULL
