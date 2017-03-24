76a3c89e1926efa71bf2452e02f9b6952894f1d3
Madison MacNichol <madimac711@gmail.com>
2017-03-24 11:18:44 -0700

    finish library drill

--- SQL/Library_Tutorial/Queries.sql
+++ SQL/Library_Tutorial/Queries.sql
@@ -13,7 +13,7 @@ FROM Book INNER JOIN Book_Copies
 ON Book.BookID=Book_Copies.BookID
 INNER JOIN Library_Branch
 ON Book_Copies.BranchID=Library_Branch.BranchID
-WHERE Library_Branch.BranchID=1 AND Book.BookID=1
+WHERE Library_Branch.BranchName='Sharptown' AND Book.Title='The Lost Tribe'
 
 /*A: There are 4 copies of The Lost Tribe at the Sharptown branch library*/
 
@@ -76,9 +76,13 @@ FROM Library_Branch
 SELECT*
 FROM Book_Loans
 
+SELECT COUNT (*) AS Total_Books, Library_Branch.BranchName
+FROM Library_Branch INNER JOIN Book_Loans
+ON Library_Branch.BranchID=Book_Loans.BranchID
+GROUP BY Library_Branch.BranchName
 
 
-/*A: */
+/*A: There are currently 13 books on loan from each library branch*/
 
 /*Q6: 6. Retrieve the names, addresses, and number of books checked out for all borrowers who have more
 than five books checked out.*/
@@ -98,9 +102,42 @@ HAVING COUNT (Borrower.CardNo)>5
 
 /*Q7: For each book authored (or co-authored) by "Stephen King", retrieve the title and the number of
 copies owned by the library branch whose name is "Central".*/
+ 
+SELECT*
+FROM Book
+SELECT*
+FROM Book_Authors
+SELECT*
+FROM Library_Branch
+SELECT*
+FROM Book_Copies
 
+SELECT Book.Title,Book_Copies.No_Of_Copies
+FROM Book_Authors INNER JOIN Book
+ON Book_Authors.BookID=Book.BookID
+INNER JOIN Book_Copies
+ON Book.BookID=Book_Copies.BookID
+INNER JOIN Library_Branch
+ON Book_Copies.BranchID=Library_Branch.BranchID
+WHERE Book_Authors.AuthorName='Stephen King' AND Library_Branch.BranchName='Central'
 
+/*A: Apparently there are no books written by Stephen King in the Central branch library...pity...*/
 
-/*A: */
+/**************STORED PROCEDURE QUERY FOR Q1****************/
 
-/**************STORED PROCEDURE QUERY****************/
+USE LibraryTutorial
+GO
+
+CREATE PROCEDURE dbo.uspGetNo_of_Copies @BranchName varchar(40), @Title varchar(40)
+AS
+SELECT*
+FROM Book INNER JOIN Book_Copies
+ON Book.BookID=Book_Copies.BookID
+INNER JOIN Library_Branch
+ON Book_Copies.BranchID=Library_Branch.BranchID
+WHERE Library_Branch.BranchName=@BranchName AND Book.Title=@Title
+GO
+
+--example execute
+EXEC dbo.uspGetNo_of_Copies @BranchName='Sharptown', @Title='Misery'
+GO
