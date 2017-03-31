38d3302b2e729692507cc7d918518c623b5417a6
Madison MacNichol <madimac711@gmail.com>
2017-03-31 16:52:33 -0700

    lynda tutorials

---
+++ JavaScript/js/script.js
@@ -0,0 +1,9 @@
+$(function () {
+    
+    $('.carousel').carousel({
+    interval: 2000,
+    pause: false, 
+    wrap: false,
+    });
+});
+//this is how you modify the speed and look of the carousel using JS
