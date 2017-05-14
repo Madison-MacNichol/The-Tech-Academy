ce2a74bfe5b835e5a3d362afae8b1805ad0cd93c
Madison MacNichol <madimac711@gmail.com>
2017-05-14 13:27:40 -0700

    finished final drill

--- Python/final_drills/date_time_drill.py
+++ Python/final_drills/date_time_drill.py
@@ -1,66 +1,43 @@
 import datetime
 import pytz
 
+def main():          
+    #Portland Headquarters Time
+    dt_pac = datetime.datetime.now(tz=pytz.timezone('US/Pacific'))
+    i =int(dt_pac.strftime('%H'+'%M')) #making integers into strings
+    def check_time(i):
+        if (i > 900 and i < 2100):
+            print('Its currently: '+ str(i)+' in Portland. The Portland Headquarters is open!')
+        else:
+            print('Sorry, its '+str(i)+' in Portland. The Portland Headquarters is closed.')
 
-#create list and apend %H in list
-#use this website for help! http://www.thegeekstuff.com/2013/06/python-list/?utm_source=feedly
+    #NYC Branch Time
+    dt_est = datetime.datetime.now(tz=pytz.timezone('US/Eastern'))
+    j =int(dt_est.strftime('%H'+'%M'))
+    def check_time1(j):
+        if (j > 900 and j < 2100):
+            print('Its currently: '+str(j)+' in NYC. The NYC Branch is open!')
+        else:
+            print('Sorry, its '+str(j)+' in NYC. The NYC Branch is closed.')
 
-city_list = ['Portland Headquarters','NYC Branch','London Branch']
-print(city_list[2]) #just to test if it worked...and it did :)
+    #London Branch Time
+    dt_lon = datetime.datetime.now(tz=pytz.timezone('Europe/London'))
+    k =int(dt_lon.strftime('%H'+'%M'))
+    def check_time2(k):
+        if (k > 900 and k < 2100):
+            print('Its currently: '+str(k)+' in London. The London Branch is open!')
+        else:
+            print('Sorry, its '+str(k)+' in London. The London Branch is closed.')
 
+    check_time(i)
+    check_time1(j)
+    check_time2(k)
 
-#these are my timezomes
-dt_pac = datetime.datetime.now(tz=pytz.timezone('US/Pacific'))
-print(dt_pac.strftime('%H'))
-pac_time =int(dt_pac.strftime('%H')) #make sure the values you're appending are int not string
-
-
-dt_est = datetime.datetime.now(tz=pytz.timezone('US/Eastern'))
-print(dt_est.strftime('%H'))
-est_time =int(dt_est.strftime('%H')) 
-
-dt_lon = datetime.datetime.now(tz=pytz.timezone('Europe/London'))
-print(dt_lon.strftime('%H'))
-lon_time =int(dt_lon.strftime('%H')) 
-
-#this appends time into city_list?
-city_list.append(['Portland Headquarters', pac_time])
-city_list.append(['NYC Branch', est_time])
-city_list.append(['London Banch', lon_time])
-
-#convert strings into integers int(dt_pac)
-
-for i in list:
-    if (i > 9 and i < 21):
-        print(i +'open')
-    else:
-        print(i+'closed')
-
-
-
+if __name__ == "__main__":
+    main()
+    
 
 
 
-
-
-
-
-##dt_utcnow = datetime.datetime.now(tz=pytz.UTC)
-##print(dt_utcnow)
-
-#dt_mtn = datetime.datetime.now()#gets time now
-#print(dt_mtn)
-
-#dt_mtn = dt_utcnow.astimezone(pytz.timezone('US/Mountain'))
-#print(dt_mtn)
-
-#for tz in pytz.all_timezones:
-#    print(tz)
-
-##dt_utcnow = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
-##print(dt_utcnow)
-##
-##dt = datetime.datetime(2016,7,27,12,30,45,tzinfo=pytz.UTC)
-##print(dt)
 
 
