413016d60ace4c25ccb72f92cd659c4319272844
Madison MacNichol <madimac711@gmail.com>
2017-06-09 11:39:13 -0700

    finished  xman challenge

--- C#/XmanChallenge/XmanChallenge/Default.aspx.cs
+++ C#/XmanChallenge/XmanChallenge/Default.aspx.cs
@@ -9,13 +9,40 @@ namespace XmanChallenge
 {
 	public partial class Default : System.Web.UI.Page
 	{
+		//Wolverine has fewist & Pheonix has most battles
+
 		protected void Page_Load(object sender, EventArgs e)
 		{
+			// Wolverine fewest battles
+			// Pheonix most battles
+
+			string[] names = new string[] { "Professor X", "Iceman", "Angel", "Beast", "Pheonix", "Cyclops", "Wolverine", "Nightcrawler", "Storm", "Colossus" };
+			int[] numbers = new int[] { 7, 9, 12, 15, 17, 13, 2, 6, 8, 13 };
+
 			string result = "";
 
-			for (int i = 0; i < 10; i++)
-				result += "<br />" + i.ToString();
+			int largestNumberIndex = 0;
+			int smallestNumberIndex = 0;
 
+
+			for (int i = 0; i < names.Length; i++)
+			{
+				// Is this the largest count so far?
+				if (numbers[i] > numbers[largestNumberIndex])
+				{
+					largestNumberIndex = i;
+				}
+
+				// Is this the smallest count so far?
+				if (numbers[i] < numbers[smallestNumberIndex])
+				{
+					smallestNumberIndex = i;
+				}
+
+			}
+
+			result = String.Format("Most battles belong to: {0} (Value: {1})", names[largestNumberIndex], numbers[largestNumberIndex]);
+			result += String.Format("<br />Least battles belong to: {0} (Value: {1})", names[smallestNumberIndex], numbers[smallestNumberIndex]);
 
 			resultLabel.Text = result;
 		}
