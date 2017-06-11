b25d2e71fd4d635262c61b47aab8c749ca56b109
Madison MacNichol <madimac711@gmail.com>
2017-06-10 17:02:35 -0700

    casino challenge

---
+++ C#/CasinoChallenge/CasinoChallenge/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CasinoChallenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        	<asp:Image ID="Image1" runat="server" Height="150px" Width="150px" />
+			<asp:Image ID="Image2" runat="server" Height="150px" Width="150px" />
+			<asp:Image ID="Image3" runat="server" Height="150px" Width="150px" />
+			<br />
+			<br />
+			Your Bet:
+			<asp:TextBox ID="betTextBox" runat="server"></asp:TextBox>
+			<br />
+			<br />
+			<asp:Button ID="pullButton" runat="server" OnClick="pullButton_Click" Text="Pull the Lever" />
+			<br />
+			<br />
+			<asp:Label ID="resultLabel" runat="server"></asp:Label>
+			<br />
+			<br />
+			<asp:Label ID="moneyLabel" runat="server"></asp:Label>
+			<br />
+			<br />
+			<br />
+			1 Cherry - 2x your bet<br />
+			2 Cherries - 3x your bet<br />
+			3 Cherries - 4x your bet<br />
+			3 7&#39;s - JACKPOT! -100x your bet<br />
+			1 Bar - you win nothing</div>
+    </form>
+</body>
+</html>
---
+++ C#/CasinoChallenge/CasinoChallenge/Default.aspx.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CasinoChallenge
+{
+	public partial class Default : System.Web.UI.Page
+	{
+		// This is out here so that each image is individually chosen at random
+		Random random = new Random();
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (!Page.IsPostBack)
+			{
+				// Display reel values
+				string[] reels = new string[] { spinReel(), spinReel(), spinReel() };
+				displayImages(reels);
+				ViewState.Add("PlayersMoney", 100);
+				displayPlayersMoney();
+			}
+
+
+		}
+
+		protected void pullButton_Click(object sender, EventArgs e)
+		{
+			// Display random image in image1 place upon button click
+			int bet = 0;
+			if (!int.TryParse(betTextBox.Text, out bet)) return;
+
+			int winnings = pullLever(bet);
+			displayResult(bet, winnings);
+			adjustPlayersMoney(bet, winnings);
+			displayPlayersMoney();
+
+		}
+
+		private void adjustPlayersMoney(int bet, int winnings)
+		{
+			int playersMoney = int.Parse(ViewState["PlayersMoney"].ToString());
+			playersMoney -= bet;
+			playersMoney += winnings;
+			ViewState["PlayersMoney"] = playersMoney;
+
+		}
+
+		private int pullLever(int bet)
+		{
+			string[] reels = new string[] { spinReel(), spinReel(), spinReel() };
+			displayImages(reels);
+
+			int multiplier = evaluateSpin(reels);
+			return bet * multiplier;
+		}
+
+		private int evaluateSpin(string[] reels)
+		{
+			// If 1 bar appears then return 0
+			if (isBar(reels)) return 0;
+
+			// If there are 3 7's then return 100
+			if (isJackpot(reels)) return 100;
+
+			// If there is one or more cherries, return 2,3,4
+			int multiplier = 0;
+			if (isWinner(reels, out multiplier)) return multiplier;
+
+			return 0;
+			
+		}
+
+		private bool isWinner(string[] reels, out int multiplier)
+		{
+			multiplier = determineMultiplier(reels);
+			if (multiplier > 0) return true;
+			return false;
+		}
+
+		private int determineMultiplier(string[] reels)
+		{
+			int cherryCount = determineCherryCount(reels);
+			if (cherryCount == 1) return 2;
+			if (cherryCount == 2) return 3;
+			if (cherryCount == 3) return 4;
+			return 0;
+		}
+
+		private int determineCherryCount(string[] reels)
+		{
+			int cherryCount = 0;
+			if (reels[0] == "Cherry") cherryCount++;
+			if (reels[1] == "Cherry") cherryCount++;
+			if (reels[2] == "Cherry") cherryCount++;
+			return cherryCount;
+		}
+		private bool isJackpot(string[] reels)
+		{
+			if (reels[0] == "Seven" && reels[1] == "Seven" && reels[2] == "Seven")
+				return true;
+			else
+				return false;
+		}
+
+		private bool isBar(string[] reels)
+		{
+			if (reels[0] == "Bar" || reels[1] == "Bar" || reels[2] == "Bar")
+				return true;
+			else
+				return false;
+		}
+
+		private string spinReel()
+		{
+			string[] images = new string[] { "Bar", "Bell", "Cherry", "Clover", "Diamond", "HorseShoe", "Lemon", "Orange", "Plum", "Seven", "Strawberry", "Watermelon" };
+			return images[random.Next(11)];
+		}
+
+		private void displayImages(string[] reels)
+		{
+			Image1.ImageUrl = "/Images/" + reels[0] + ".png";
+			Image2.ImageUrl = "/Images/" + reels[1] + ".png";
+			Image3.ImageUrl = "/Images/" + reels[2] + ".png";
+		}
+
+		private void displayResult(int bet, int winnings)
+		{
+			if (winnings > 0)
+				resultLabel.Text = string.Format("You bet {0:C} and won {1:C}", bet, winnings);
+			else
+				resultLabel.Text = string.Format("Sorry you lost {0:C}! Better luck next time!", bet);
+		}
+
+		private void displayPlayersMoney()
+		{
+			moneyLabel.Text = string.Format("Player's Money: {0:C}", ViewState["PlayersMoney"]);
+		}
+	}
+}
