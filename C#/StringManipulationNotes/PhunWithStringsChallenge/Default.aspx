3168ac7759d963ec5ac8a493d92460af6398f8e1
Madison MacNichol <madimac711@gmail.com>
2017-06-14 10:42:59 -0700

    nearly done hero/monster challenge

---
+++ C#/StringManipulationNotes/PhunWithStringsChallenge/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PhunWithStringsChallenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:TextBox ID="valueTextBox" runat="server"></asp:TextBox>
+		<br />
+		<br />
+		<asp:Button ID="okButton" runat="server" OnClick="okButton_Click" style="height: 26px" Text="OK" />
+		<br />
+		<br />
+		<asp:Label ID="resultLabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ C#/StringManipulationNotes/PhunWithStringsChallenge/Default.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PhunWithStringsChallenge
+{
+	public partial class Default : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void okButton_Click(object sender, EventArgs e)
+		{
+			//Back slashes are used to let intellisense know what were trying to do here
+			//resultLabel.Text = "<p style=\"color:#ee3b32;\">HI</p>";
+
+			//string value = valueTextBox.Text;
+
+			//Access any specific character
+			//resultLabel.Text = value[2].ToString();
+
+			//Can also use EndsWith() OR Contains()
+			/*if (value.StartsWith("A"))
+				resultLabel.Text = "The value starts with A";*/
+
+			//Shows index of something (handy to know for insert/remove statements)
+			//int index = value.IndexOf("good");
+			//resultLabel.Text = "'good' begins at index" + index.ToString();
+
+			//Insert/Remove
+			//resultLabel.Text = value.Insert(index, "jolly");
+			//resultLabel.Text = value.Remove(index, value.Length - index);
+
+			//Substrings
+			//resultLabel.Text = value.Substring(index, value.Length - index - 1);
+
+			//Trim (to trim on any empty characters)
+			//resultLabel.Text = string.Format("Length before : {0}<br/>Length after : {1}", value.Length, value.Trim().Length);
+
+			//PadLeft/PadRight
+			//resultLabel.Text = value.PadLeft(10, '*');
+
+			//ToUpper /ToLower (chages upper and lower casing in strings)
+			//if (valueTextBox.Text.Trim().ToUpper() == "BOB") resultLabel.Text = "The Same";
+			//else resultLabel.Text = "Different";
+
+			//Replace
+			//string template = "[NAME] said ok! Take it up with [NAME].";
+			//resultLabel.Text = template.Replace("[NAME]", valueTextBox.Text);
+
+			//Split (splits up 1 string into an array of strings)
+			//string result = ""; OR
+			//StringBuilder sb = new StringBuilder();
+
+			//string[] values = valueTextBox.Text.Split(',');
+			/*for (int i = 0; i < values.Length; i++)
+			{
+				//result += values[i] + " " + values[i].Length + "<br/>"; OR
+				sb.Append(values[i]);
+				sb.Append(" ");
+				sb.Append(values[i].Length);
+				sb.Append("<br />");
+			}*/
+
+			//resultLabel.Text = result; OR
+			//resultLabel.Text = sb.ToString();
+
+
+		}
+	}
+}
