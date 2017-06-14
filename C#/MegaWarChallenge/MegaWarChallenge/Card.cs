e809daa3adbc2c1916c14b5fe0a0ad933bed3a30
Madison MacNichol <madimac711@gmail.com>
2017-06-14 15:49:10 -0700

    first part of mega war challenge

---
+++ C#/MegaWarChallenge/MegaWarChallenge/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MegaWarChallenge
+{
+	public class Card
+	{
+		public string Suit { get; set; }
+		public string Kind { get; set; }
+	}
+}
---
+++ C#/MegaWarChallenge/MegaWarChallenge/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MegaWarChallenge
+{
+	public partial class Default : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void playButton_Click(object sender, EventArgs e)
+		{
+			//This was a test
+			/*Deck deck = new Deck();
+			foreach (var card in deck._deck)
+			{
+				resultLabel.Text += "<br/>" + card.Suit + " " + card.Kind;
+			}*/
+
+			Game game = new Game("Player1", "Player2");
+			resultLabel.Text = game.Play();
+		}
+	}
+}
